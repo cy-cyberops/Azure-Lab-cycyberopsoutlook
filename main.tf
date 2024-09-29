42c488e09b766f6e17a58b754a585b285deff087
Yogesh Chaurasia <cy.cyberops@gmail.com>
2024-09-29 16:13:37 +0530

    test hc resource group

--- main.tf
+++ main.tf
@@ -52,6 +52,15 @@ resource "azurerm_resource_group" "rg_irtech_prod" {
 
 }
 
+resource "azurerm_resource_group" "rg_hc_prod" {
+  name     = "rg-hc-prod"
+  location = "East US"
+  tags = {
+    environment = "prod"
+  }
+
+}
+
 resource "azurerm_virtual_network" "vnet_desco" {
   name                = "vnet-desco"
   location            = "East US"
