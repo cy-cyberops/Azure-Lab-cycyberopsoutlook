30ecb3dfb411b1febf5d9da180e8c3a9165fdded
Yogesh Kumar Chaurasia <134851605+cy-cyberops@users.noreply.github.com>
2024-09-29 15:54:56 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,114 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.113.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  subscription_id = "6bf4720d-ba5b-46c2-a93a-c5c109d0399f"
+  client_id       = "f82c4e84-648c-487d-a52d-489ca6a1eb00"
+  client_secret   = "WHm8Q~tW3fWhSRCv0ryfB6v-1kjHg30YAjmWdc2l"
+  tenant_id       = "768ae5da-7689-4480-9bcd-65e61ea05f6a"
+
+  features {}
+}
+
+resource "azurerm_resource_group" "rg_irtech" {
+  name     = "rg-irtech"
+  location = "East US"
+  tags = {
+    environment = "dev"
+  }
+
+}
+
+resource "azurerm_resource_group" "rg_desco" {
+  name     = "rg-desco"
+  location = "East US"
+  tags = {
+    environment = "dev"
+  }
+
+}
+
+resource "azurerm_resource_group" "rg_irtech_dev" {
+  name     = "rg-irtech-dev"
+  location = "East US"
+  tags = {
+    environment = "dev"
+  }
+
+}
+resource "azurerm_resource_group" "rg_irtech_prod" {
+  name     = "rg-irtech-prod"
+  location = "East US"
+  tags = {
+    environment = "prod"
+  }
+
+}
+
+resource "azurerm_virtual_network" "vnet_desco" {
+  name                = "vnet-desco"
+  location            = "East US"
+  resource_group_name = "rg-desco"
+  address_space       = ["10.0.0.0/8", "192.168.0.0/16", "172.16.0.0/12"]
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_subnet" "subnet_ext_dmz_irtech_0" {
+  name                 = "subnet-ext-dmz-irtech-0"
+  resource_group_name  = "rg-desco"
+  virtual_network_name = "vnet-desco"
+  address_prefixes     = ["192.168.10.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
+}
+
+
+resource "azurerm_network_security_group" "sg_vnet-desco" {
+  name                = "nsg-vnet-desco"
+  location            = azurerm_resource_group.rg_desco.location
+  resource_group_name = azurerm_resource_group.rg_desco.name
+}
+
+resource "azurerm_network_security_rule" "sr_allow-inbound-access" {
+
+  name                        = "sr-allow-inbound-access"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg_desco.name
+  network_security_group_name = azurerm_network_security_group.sg_vnet-desco.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "snsga_subnet-ext-dmz-irtech-0_sg-vnet-desco" {
+  subnet_id                 = azurerm_subnet.subnet_ext_dmz_irtech_0.id
+  network_security_group_id = azurerm_network_security_group.sg_vnet-desco.id
+}
+
+
+
+
+
+
+
