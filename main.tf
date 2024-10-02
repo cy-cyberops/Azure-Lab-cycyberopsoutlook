414c32a4ba5f95fb93179d8fc25b6e81f5863206
Yogesh Chaurasia <cy.cyberops@gmail.com>
2024-10-02 19:40:03 +0530

    add azurerm

--- main.tf
+++ main.tf
@@ -1,12 +1,15 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.113.0"
+      source = "hashicorp/azurerm"
+      version = "4.3.0"
     }
   }
 }
 
+provider "azurerm" {
+  # Configuration options
+}
 
 resource "azurerm_resource_group" "rg_irtech" {
   name     = "rg-irtech"
